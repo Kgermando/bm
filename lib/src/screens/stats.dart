d3dc16c814b724e3fe96369d7aa62b9ad1ca5448
Germain Kataku <katakugermain@gmail.com>
2021-06-21 08:11:15 +0100

    add stats

---
+++ lib/src/screens/stats.dart
@@ -0,0 +1,31 @@
+import 'package:e_management/src/screens/sidebar_screen.dart';
+import 'package:flutter/material.dart';
+
+class StatistiqueWiddget extends StatefulWidget {
+  const StatistiqueWiddget({ Key? key }) : super(key: key);
+
+  @override
+  _StatistiqueWiddgetState createState() => _StatistiqueWiddgetState();
+}
+
+class _StatistiqueWiddgetState extends State<StatistiqueWiddget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text('Statistiques'),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.power_settings_new),
+              label: Text(''),
+            ),
+          ],
+        )
+      ),
+      drawer: SideBarScreen(),
+    );
+  }
+}

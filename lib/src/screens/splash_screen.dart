a923675baeb06d310d82440e31fd352c15abbf83
Germain Kataku <katakugermain@gmail.com>
2021-06-25 20:13:39 +0100

    splash dart

---
+++ lib/src/screens/splash_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenWidget extends StatefulWidget {
+  const SplashScreenWidget({ Key? key }) : super(key: key);
+
+  @override
+  _SplashScreenWidgetState createState() => _SplashScreenWidgetState();
+}
+
+class _SplashScreenWidgetState extends State<SplashScreenWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

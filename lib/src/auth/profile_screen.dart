91da154f4f1dbcf63d06fdbed0eb2f57af45dc65
Germain Kataku <katakugermain@gmail.com>
2021-08-14 19:18:02 +0100

    profile done

--- lib/src/auth/profile_screen.dart
+++ lib/src/auth/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:e_management/services/user_preferences.dart';
 import 'package:e_management/src/models/user_model.dart';
+import 'package:e_management/themes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -17,38 +19,82 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         title: Text('User Info'),
       ),
-      body: Center(
-        child: FutureBuilder<User?>(
-          future: UserPreferences.read(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              User? userInfo = snapshot.data;
-              if (userInfo != null) {
-                var userData = userInfo;
-                return Column(
+      body: FutureBuilder<User?>(
+        future: UserPreferences.read(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            User? userInfo = snapshot.data;
+            if (userInfo != null) {
+              var userData = userInfo;
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
                   mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    // Image.network(userData.avatar),
+                    Image.asset("assets/images/profile.jpg", height: 150.0, width: 150.0, fit: BoxFit.cover,),
                     SizedBox(height: 8.0),
                     Text(
-                      '${userInfo.firstName} ${userInfo.lastName}',
-                      style: TextStyle(fontSize: 16.0),
+                      '${userData.firstName} ${userData.lastName}',
+                      style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w800),
                     ),
-                    Text(
-                      userData.email,
-                      style: TextStyle(fontSize: 16.0),
+                    SizedBox(height: 16.0,),
+                    
+                    Row(
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.phone,
+                                size: 40.0, color: MyThemes.primary)),
+                        Spacer(),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.business,
+                                size: 40.0, color: MyThemes.primary)),
+                        Spacer(),
+                        IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.money,
+                                size: 40.0, color: MyThemes.primary))
+                      ],
                     ),
+
+                    SizedBox(
+                      height: 10.0,
+                    ),
+
+                    Row(
+                      children: [
+                        Text(
+                          '${userData.telephone}',
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.w800),
+                        ),
+                        Spacer(),
+                        Text(
+                          '${userData.nameBusiness}',
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.w800),
+                        ),
+                        Spacer(),
+                        Text(
+                          '${userData.typeAbonnement}',
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.w800),
+                        ),
+                      ],
+                    ),
+                    
                   ],
-                );
-              }
+                ),
+              );
             }
-            return CircularProgressIndicator();
-            
-          },
-        ),
+          }
+          return CircularProgressIndicator();
+          
+        },
       ),
-
-
     );
   }
 }

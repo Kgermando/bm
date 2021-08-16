ee209684b4a2e86b6fabc3586acb61529b2fa4b4
Germain Kataku <katakugermain@gmail.com>
2021-08-16 22:01:25 +0100

    ss

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -1,7 +1,7 @@
 
 class Environment {
-  // static String serverUrl = "https://business-management.herokuapp.com/api";
-  static String serverUrl = "http://192.168.43.230:3000/api";
+  static String serverUrl = "https://business-management.herokuapp.com/api";
+  // static String serverUrl = "http://192.168.43.230:3000/api";
 
   var loginUrl = Uri.parse("$serverUrl/auth/login");
   var registerUrl = Uri.parse("$serverUrl/auth/register");
